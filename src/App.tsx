1b75280705fd771a39c8042c46618b2bc8b4c7e3
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-06 00:11:13 +0600

    space

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,26 @@ import { useDispatch, useSelector } from "react-redux";
 import './App.css';
 import { selectTodo } from './redux/store';
 import { addTodo, removeTodo } from "./redux/TodoSlice";
+
 function App() {
+
   const dispatch = useDispatch();
+
   const todos = useSelector(selectTodo);
-  const todoRef = useRef<HTMLInputElement>(null)
+
+  const todoRef = useRef<HTMLInputElement>(null);
+
   const createTodo = () => {
     if (todoRef.current) {
       dispatch(addTodo(todoRef.current.value));
       todoRef.current.value = "";
     }
-  }
+  };
+
   const deleteTodo = (id: number) => {
     dispatch(removeTodo(id))
-  }
+  };
+
   return (
     <div>
       {

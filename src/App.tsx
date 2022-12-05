347001a02ec6495dcc2e83adb54697c1d68d0091
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-05 21:12:32 +0600

    deleteTodo

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,32 @@ import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import './App.css';
 import { selectTodo } from './redux/store';
-import { addTodo } from "./redux/TodoSlice";
+import { addTodo, removeTodo } from "./redux/TodoSlice";
 function App() {
   const dispatch = useDispatch();
   const todos = useSelector(selectTodo);
   const todoRef = useRef<HTMLInputElement>(null)
   const createTodo = () => {
     if (todoRef.current) {
-      dispatch(addTodo(todoRef.current.value))
+      dispatch(addTodo(todoRef.current.value));
+      todoRef.current.value = "";
     }
   }
-  console.log(todos);
+  const deleteTodo = (id: number) => {
+    dispatch(removeTodo(id))
+  }
   return (
     <div>
+      {
+        todos && todos.map((todo) =>
+        (
+          <div key={todo.id}>
+            <h4>{todo.text}</h4>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </div>
+        )
+        )
+      }
       <input type="text" ref={todoRef} />
       <button onClick={createTodo}>Add Todo</button>
     </div>
